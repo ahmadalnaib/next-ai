1497bb2c79d21d6e9b34da2611c7f195f154eb84
ahmad.al-naib <alnaib888@gmail.com>
2024-07-17 15:17:35 +0200

    add

--- chatgpt/app/api/create-chat-completion/route.js
+++ chatgpt/app/api/create-chat-completion/route.js
@@ -1,22 +1,32 @@
-import { NextResponse } from "next/server";
-import OpenAI from "openai";
+import { NextResponse } from 'next/server';
+import OpenAI from 'openai';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 export async function POST(req) {
-  if(!openai.apiKey) {
-    return NextResponse.error(new Error("API key not provided"), { status: 500 });
+  if (!openai.apiKey) {
+    return NextResponse.error(new Error('API key not provided'), {
+      status: 500,
+    });
   }
   const body = await req.json();
 
-  const completion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: body.messages,
-  });
-  console.log(completion.choices[0].message);
-  const theResponse = completion.choices[0].message;
+  try {
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: body.messages,
+    });
 
-  return NextResponse.json({ output: theResponse }, { status: 200 });
+    const theResponse = completion.choices[0].message;
+
+    return NextResponse.json({ output: theResponse }, { status: 200 });
+  } catch (error) {
+    if (error.response.status == 401) {
+      return NextResponse.json({ error: 'Invalid API key' }, { status: 401 });
+    }  else {
+    console.error(error.response.status);
+    }
+  }
 }
